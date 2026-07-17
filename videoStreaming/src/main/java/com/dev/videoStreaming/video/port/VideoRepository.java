32cde6653890f37fd99c3bd1e394a177cdf60b65
EL-ABAD <walidelabad@gmail.com>
2026-07-17 15:40:57 +0100

    stashed commit

--- videoStreaming/src/main/java/com/dev/videoStreaming/video/port/VideoRepository.java
+++ videoStreaming/src/main/java/com/dev/videoStreaming/video/port/VideoRepository.java
@@ -1,10 +1,14 @@
 package com.dev.videoStreaming.video.port;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.dev.videoStreaming.video.domain.videoMetadata;
 import java.util.Optional;
 
-public interface videoRepository extends JpaRepository<videoMetadata, Long> {
+public interface VideoRepository extends JpaRepository<videoMetadata, Long> {
     Optional<videoMetadata> findByObjectName(String objectName);
     Optional<videoMetadata> findByVideoId(String videoId);
+    Page<videoMetadata> findByObjectNameContaining(String search , Pageable pageable);
+
 }
